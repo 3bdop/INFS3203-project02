b7e86d607ece3f8ed58f8f2674a73b063baf09b2
abood1509 <abdbinm55@gmail.com>
2024-03-19 23:46:30 +0300

    added the config file, added app logo, add the starter page

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name='Main' component={Main} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ components/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+// import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEETNjdJkw230OMMrncEbf7bPPxOiFmW8",
+  authDomain: "infs3203-project.firebaseapp.com",
+  projectId: "infs3203-project",
+  storageBucket: "infs3203-project.appspot.com",
+  messagingSenderId: "471484750231",
+  appId: "1:471484750231:web:d864e29c730afaae3c4cd2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app)
+// export const storage = getStorage(app);
