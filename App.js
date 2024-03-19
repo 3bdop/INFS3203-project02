f5f78b34be093190e1a56a439058728cfc4efa77
abood1509 <abdbinm55@gmail.com>
2024-03-19 23:49:36 +0300

    added the config file, added app logo, add the main page

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Main from './components/Main'
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator();
 
