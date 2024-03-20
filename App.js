20a8927e575b4699698b24234e1791ab28a4b415
Mbii55 <mbii5567@hotmail.com>
2024-03-20 22:34:30 +0300

    Added a Tab Navigation function for the main app pages

--- App.js
+++ App.js
@@ -5,15 +5,30 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Main from './components/Main'
 import LoginScreen from './components/LoginScreen'
+import Home from './components/Home';
+import RegistarScreen from './components/RegistarScreen';
+
+
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      {/* <Tab.Screen name="Settings" component={Settings} options={{ headerShown: false }} /> */}
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Main">
         <Stack.Screen name='Main' component={Main} options={{ headerShown: false }} />
-        <Stack.screen name='LoginScreen' component={LoginScreen} options={{ headerShown: false}} />
+        <Stack.Screen name='LoginScreen' component={LoginScreen} options={{ headerShown: false}} />
+        <Stack.Screen name='RegistarScreen' component={RegistarScreen} options={{ headerShown: false}} />
+        <Stack.Screen name='MainTabs' component={MainTabs} options={{ headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
