6cd2cd57931d90680893bf75ebbf17299662db39
abood1509 <abdbinm55@gmail.com>
2024-03-26 22:37:00 +0300

    added the edit profile screen with the functionality

--- components/EditProfile.js
+++ components/EditProfile.js
@@ -4,15 +4,18 @@ import { AntDesign, Entypo } from 'react-native-vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Avatar, Card } from "@rneui/themed";
 import * as ImagePicker from 'expo-image-picker';
-
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { addDoc, doc, collection, getDoc, getDocs, setDoc, query, where, updateDoc } from "firebase/firestore";
+import { auth, db, storage } from "./config";
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
 
-const EditProfile = ( {navigation} ) => {
-
+const EditProfile = ({ navigation, route }) => {
+    const { email } = route.params
     const [image, setImage] = useState(null)
-
+    const [newName, setNewName] = useState()
+    const [isLoading, setIsLoading] = useState(false)
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -23,69 +26,111 @@ const EditProfile = ( {navigation} ) => {
         }
     }
 
-  return (
-    <SafeAreaView style={styles.container}>
+    const uploadImage = async (docId) => {
+        if (!image || !docId) return null; // Check if there's an image and a document ID
 
-        <ScrollView>
+        const imgRef = ref(storage, `profile_pictures/${docId}`);
 
-        
-        <View style={styles.avatarContainer}>
-            <Avatar size={'xlarge'}rounded source={image ? {uri:image} : require('../assets/user.jpg')}/>
-        </View>
+        try {
+            const imgResponse = await fetch(image);
+            const imgBlob = await imgResponse.blob();
 
-        {/* Change Pic Botton Container */}
-        <View style={styles.changePicBottonContainer}>
+            // Upload the image
+            await uploadBytesResumable(imgRef, imgBlob);
 
-            <TouchableOpacity style={styles.changePicButton} onPress={pickImage}>
-                <Text style={styles.changePicTxt}>Change Picture</Text>
-            </TouchableOpacity>
+            // Once uploaded, get the download URL
+            const downloadURL = await getDownloadURL(imgRef);
 
-        </View>
+            console.log('Image uploaded successfully. Download URL:', downloadURL);
 
-        {/* User Info */}
-        <View style={styles.userInfo}>
+            return downloadURL; // Return the download URL
+        } catch (error) {
+            console.error("Error uploading image: ", error);
+            return null; // Return null in case of error
+        }
+    };
+    const store = async () => {
+        try {
+            // Get a reference to the document with the email ID
+            const docRef = doc(db, "users", email);
 
-            {/* Name */}
-            <View style={styles.userInfoHeaderContainer}>
-                <Text style={styles.headerTxt}>Name</Text>
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput placeholder='Mohamed' style={styles.input}/> 
-            </View>
+            // Upload the image and get the URL
+            const imageUrl = await uploadImage(email);
 
-            {/* Email ID */}
-            <View style={styles.userInfoHeaderContainer}>
-                <Text style={styles.headerTxt}>Email ID</Text>
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput inputMode='email' placeholder='Exampl@hotmail.com' style={styles.input}/>
-            </View>
-        </View>
+            // Update the document with the new name and image
+            await updateDoc(docRef, { name: newName, pic: imageUrl });
 
-         {/* Update Button */}
-         <View style={styles.UpdateButtonContainer}>
-            <TouchableOpacity style={styles.UpdateButton}>
-                <Text style={styles.UpdateTxt}>Update</Text>
-            </TouchableOpacity>
-        </View>
-        
-        
-        </ScrollView>
-    </SafeAreaView>
-  )
+            console.log("Document updated with ID: ", email);
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+
+    };
+
+    const handleAll = async () => {
+        setIsLoading(true)
+        const tempID = await store()
+        setIsLoading(false)
+        navigation.navigate("Home")
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+
+            <ScrollView>
+
+
+                <View style={styles.avatarContainer}>
+                    <Avatar size={'xlarge'} rounded source={image ? { uri: image } : require('../assets/user.jpg')} />
+                </View>
+
+                {/* Change Pic Botton Container */}
+                <View style={styles.changePicBottonContainer}>
+
+                    <TouchableOpacity style={styles.changePicButton} onPress={pickImage}>
+                        <Text style={styles.changePicTxt}>Change Picture</Text>
+                    </TouchableOpacity>
+
+                </View>
+
+                {/* User Info */}
+                <View style={styles.userInfo}>
+
+                    {/* Name */}
+                    <View style={styles.userInfoHeaderContainer}>
+                        <Text style={styles.headerTxt}>Name</Text>
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <TextInput placeholder='Mohamed' style={styles.input} />
+                    </View>
+
+                    {/* Email ID */}
+
+                </View>
+
+                {/* Update Button */}
+                <View style={styles.UpdateButtonContainer}>
+                    <TouchableOpacity style={styles.UpdateButton} onPress={handleAll}>
+                        <Text style={styles.UpdateTxt}>Update</Text>
+                    </TouchableOpacity>
+                </View>
+
+
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 export default EditProfile
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         backgroundColor: '#e6e6e6',
         flex: 1,
         alignItems: 'center'
     },
 
     // Avatar Container
-    avatarContainer:{
+    avatarContainer: {
         // backgroundColor: 'tomato',
         width: screenWidth,
         height: screenHeight * 0.23,
@@ -94,14 +139,14 @@ const styles = StyleSheet.create({
     },
 
     // Change Pic Button
-    changePicBottonContainer:{
+    changePicBottonContainer: {
         // backgroundColor: 'gray',
         width: screenWidth,
         height: screenHeight * 0.1,
         justifyContent: 'center',
         alignItems: 'center'
     },
-    changePicButton:{
+    changePicButton: {
         backgroundColor: '#2F7694',
         width: '45%',
         height: '50%',
@@ -109,18 +154,18 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    changePicTxt:{
+    changePicTxt: {
         color: 'white'
     },
 
     // User Information
-    userInfo:{
+    userInfo: {
         // backgroundColor: 'pink',
         width: screenWidth,
         height: screenHeight * 0.4,
 
     },
-    userInfoHeaderContainer:{
+    userInfoHeaderContainer: {
         // backgroundColor: 'gray',
         width: '100%',
         // height: '8%',
@@ -128,16 +173,16 @@ const styles = StyleSheet.create({
         paddingLeft: 20,
         justifyContent: 'center'
     },
-    headerTxt:{
+    headerTxt: {
         fontSize: 17,
         paddingBottom: 5
     },
-    inputContainer:{
+    inputContainer: {
         // backgroundColor: 'yellow',
         height: '15%',
         paddingLeft: 20,
     },
-    input:{
+    input: {
         width: '90%',
         height: '85%',
         backgroundColor: 'white',
@@ -148,7 +193,7 @@ const styles = StyleSheet.create({
     },
 
     // Sign Up Button
-    UpdateButtonContainer:{
+    UpdateButtonContainer: {
         // backgroundColor: 'lightgray',
         width: screenWidth,
         height: screenHeight * 0.1,
@@ -157,7 +202,7 @@ const styles = StyleSheet.create({
     },
 
     // Update Button
-    UpdateButton:{
+    UpdateButton: {
         width: '80%',
         height: '60%',
         backgroundColor: '#2F7694',
@@ -165,7 +210,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    UpdateTxt:{
+    UpdateTxt: {
         fontSize: 16,
         fontWeight: 'bold',
         color: 'white'
--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -1,4 +1,4 @@
-import {StyleSheet,Dimensions, Image,Text, View, TouchableOpacity, Platform, TextInput,ScrollView, SafeAreaView } from 'react-native';
+import { StyleSheet, Dimensions, Image, Text, View, TouchableOpacity, Platform, TextInput, ScrollView, SafeAreaView } from 'react-native';
 import React from 'react'
 import { Feather, MaterialIcons } from 'react-native-vector-icons'
 import { Avatar, Card } from '@rneui/themed';
@@ -7,55 +7,55 @@ import { CardDivider } from '@rneui/base/dist/Card/Card.Divider';
 const screenWidth = Dimensions.get('window').width
 const screenHeight = Dimensions.get('window').height
 
-const Settings = ({navigation}) => {
-  return (
-      <SafeAreaView style={styles.container}>
+const Settings = ({ navigation }) => {
+    return (
+        <SafeAreaView style={styles.container}>
 
-        <ScrollView>
+            <ScrollView>
 
-        <View style={styles.accountContainer}>
-        {/* Header */}
-        <View style={styles.header}>
-            <Text style={styles.headerTxt}>Account</Text>
-        </View>
+                <View style={styles.accountContainer}>
+                    {/* Header */}
+                    <View style={styles.header}>
+                        <Text style={styles.headerTxt}>Account</Text>
+                    </View>
 
-        {/* Support & About options */}
-        <Card containerStyle={[styles.CardContainer, {height: screenHeight * 0.13}]}>
+                    {/* Support & About options */}
+                    <Card containerStyle={[styles.CardContainer, { height: screenHeight * 0.13 }]}>
 
-            {/* Edit Profile */}
-            <TouchableOpacity style={[styles.options, {height: '45%'}]}>
-                <Feather name={'user'} size={25} color={'#818181'}/>
-                <Text style={styles.txt}>Edit Profile</Text>
-            </TouchableOpacity>
+                        {/* Edit Profile */}
+                        <TouchableOpacity style={[styles.options, { height: '45%' }]}>
+                            <Feather name={'user'} size={25} color={'#818181'} />
+                            <Text style={styles.txt}>Edit Profile</Text>
+                        </TouchableOpacity>
 
-            <CardDivider/>
+                        <CardDivider />
 
-            {/* Log Out */}
-            <TouchableOpacity onPress={()=>navigation.replace('Main')} style={[styles.options, {height: '40%'}]}>
-                <MaterialIcons name={'logout'} size={25} color={'#818181'}/>
-                <Text style={styles.txt}>Log Out</Text>
-            </TouchableOpacity>
+                        {/* Log Out */}
+                        <TouchableOpacity onPress={() => navigation.replace('Main')} style={[styles.options, { height: '40%' }]}>
+                            <MaterialIcons name={'logout'} size={25} color={'#818181'} />
+                            <Text style={styles.txt}>Log Out</Text>
+                        </TouchableOpacity>
 
-        </Card>
-        </View>
+                    </Card>
+                </View>
 
-        </ScrollView>
-        
-    </SafeAreaView>
-  )
+            </ScrollView>
+
+        </SafeAreaView>
+    )
 }
 
 export default Settings
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         // flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#e6e6e6',
         flex: 1
     },
-    header:{
+    header: {
         width: screenWidth,
         height: screenHeight * 0.05,
         // backgroundColor: 'pink',
@@ -63,30 +63,30 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         paddingLeft: 20,
     },
-    headerTxt:{
+    headerTxt: {
         fontSize: 17,
         fontWeight: 'bold',
     },
-    accountContainer:{
+    accountContainer: {
         // backgroundColor: 'tomato',
         height: screenHeight * 0.32,
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 20,
     },
-    CardContainer:{
+    CardContainer: {
         width: screenWidth * 0.9,
         height: screenHeight * 0.23,
         backgroundColor: 'white',
         borderRadius: 7,
     },
-    options:{
+    options: {
         // backgroundColor: 'lightgray',
         height: '19%',
         flexDirection: 'row',
         alignItems: 'center',
     },
-    txt:{
+    txt: {
         fontWeight: 'bold',
         paddingLeft: 15,
 
